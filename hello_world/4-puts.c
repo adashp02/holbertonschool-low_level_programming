0740a7b361efeea7aaaafd50deba25d5d6b645a8
adashp02 <adashp02@gmail.com>
2025-02-17 05:06:05 +0000

    Write a C program that prints exactly "Programming is like building a multilingual puzzle, followed by a new line.

--- hello_world/4-puts.c
+++ hello_world/4-puts.c
@@ -1,6 +1,5 @@
 #include <stdio.h>
-/* prints a sentence 
- * newline included */
+/* prints a sentence newline included */
 int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
