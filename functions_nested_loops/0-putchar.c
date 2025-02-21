dae8d0cdeb869b53e62c279add18db159546551b
adashp02 <adashp02@gmail.com>
2025-02-21 06:09:11 +0000

    Write a program that prints _putchar, followed by a new line.

--- functions_nested_loops/0-putchar.c
+++ functions_nested_loops/0-putchar.c
@@ -1,20 +1,21 @@
 #include <stdio.h>
-#include <unistd.h> 
-#include <string.h> 
+#include <unistd.h>
+#include <string.h>
 #include "main.h"
 
 /**
- * main - print a string usig printf
+ * main - print a string usig write
  *
  * Return: 0 is a sign of success
  */
 int main(void)
 {
-	int file_descriptor = STDOUT_FILENO; 
-    	const char* message = "_putchar\n"; 
-    	size_t message_length = strlen(message); 
-    	write(file_descriptor, message, message_length); 
- 
-    return 0; 
-} 
+	int file_descriptor = STDOUT_FILENO;
+	const char *message = "_putchar\n";
+	size_t message_length = strlen(message);
 
+	write(file_descriptor, message, message_length);
+
+	return (0);
+
+}
