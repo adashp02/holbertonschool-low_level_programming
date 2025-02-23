a43a989591123471b91359b6e46f8c516a9f7762
adashp02 <adashp02@gmail.com>
2025-02-23 08:36:19 +0000

    Write a function that prints the 9 times table, starting with 0.Prototype: void times_table(void)

--- functions_nested_loops/9-times_table.c
+++ functions_nested_loops/9-times_table.c
@@ -7,53 +7,34 @@
  */
 
 void times_table(void)
-
 {
-
 	int x, y, z;
 
 	for (x = 0; x <= 9; x++)
-
 	{
-
 		for (y = 0; y <= 9; y++)
-
 		{
 			z = x * y;
 			if (y == 0)
 
 			{
 				_putchar (y + '0');
-
 			}
-
 			if (z < 10 && y != 0)
-
 			{
 				_putchar (',');
 				_putchar (' ');
 				_putchar (' ');
-				/* line below converts a digit into ASCII */
-				_putchar (z + '0');
-
+				_putchar (z + '0');/* converts a digit into ASCII */
 			}
-
 			else if (z >= 10)
-
 			{
 				_putchar (',');
 				_putchar (' ');
-				/* line below divide by 10 and converts a digit into ASCII */
 				_putchar ((z / 10) + '0');
-				/* line below modulo by 10 and converts a digit into ASCII */
 				_putchar ((z % 10) + '0');
-
 			}
-
-
 		}
-
 		_putchar ('\n');
 	}
-
 }
