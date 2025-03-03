5d65ba96c9a8e9b52e14f36e845a802524216eff
adashp02 <adashp02@gmail.com>
2025-03-03 11:26:50 +0000

    Write a function that capitalizes all words of a string.Separators of words: space, tabulation, new line, ,, ;, ., !, ?, ", (, ), {, and }

--- pointers_arrays_strings/6-cap_string.c
+++ pointers_arrays_strings/6-cap_string.c
@@ -4,29 +4,30 @@
 *@str: string to capitalize
 *Return:returns the capitalized string
 */
-char *cap_string(char *str)
+char *cap_string(char *s)
+
 {
-	int count = 0;
+	int i, j;
 
-	while (str[++count])
+	char spe[13] = {' ', '\t', '\n', ',', ';', '.',
+		'!', '?', '"', '(', ')', '{', '}'};
+
+	for (i = 0; s[i] != '\0'; i++)
 	{
-		while (!(str[count] >= 'a' && str[count] <= 'z'))
-			count++;
+		if (i == 0 && s[i] >= 'a' && s[i] <= 'z')
+			s[i] -= 32;
 
-		if (str[count - 1] == ' ' ||
-				str[count - 1] == '\t' ||
-				str[count - 1] == '\n' ||
-				str[count - 1] == ',' ||
-				str[count - 1] == ';' ||
-				str[count - 1] == '.' ||
-				str[count - 1] == '!' ||
-				str[count - 1] == '?' ||
-				str[count - 1] == '"' ||
-				str[count - 1] == '(' ||
-				str[count - 1] == ')' ||
-				str[count - 1] == '{' ||
-				str[count - 1] == '}')
-			str[count] -= 32;
+		for (j = 0; j < 13; j++)
+		{
+			if (s[i] == spe[j])
+			{
+				if (s[i + 1] >= 'a' && s[i + 1] <= 'z')
+				{
+					s[i + 1] -= 32;
+				}
+			}
+		}
 	}
-	return (str);
+
+	return (s);
 }
