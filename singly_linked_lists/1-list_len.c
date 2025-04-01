75dac84a17c4d47d02615371b30d1a7b42ccb3b7
adashp02 <adashp02@gmail.com>
2025-04-01 05:57:54 +0000

    return number of elements in a linked list

---
+++ singly_linked_lists/1-list_len.c
@@ -0,0 +1,26 @@
+#include <stdlib.h>
+#include "lists.h"
+
+/**
+ * list_len - returns amount of elements
+ * from the list list_t
+ * @h: pointer to the list_t
+ *
+ * Return: number of elements
+ */
+
+size_t list_len(const list_t *h)
+
+{
+	size_t number = 0;
+
+	while (h)
+
+	{
+		number++;
+		h = h->next;
+	}
+
+	return (number);
+
+}
